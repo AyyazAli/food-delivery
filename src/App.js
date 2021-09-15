7d2a7da00a88a18d3f4d2b0b19d81d0333b8c7b0
Ayyaz Ali Furqan <ayyazalifurqan@gmail.com>
2021-09-15 18:49:43 +0500

    fixed Routing Issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { ThemeProvider } from '@material-ui/styles';
 import routes from './routes'
-import { Router } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import RenderRoutes from './components/Routing/renderRoutes/renderRoutes';
 import theme from './theme/index'
