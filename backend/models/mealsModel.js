33df9f9dea3ad5c21548dbf070351110fe5b78fa
Ayyaz Ali Furqan <ayyazalifurqan@gmail.com>
2021-09-17 19:21:29 +0500

    Meals Backend

---
+++ backend/models/mealsModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    restaurant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'restaurant',
+        required: true
+    },
+    name: {
+        type: String,
+        required: [true, 'Meal Name is requried']
+    },
+    description: { type: String },
+    price: {
+        type: Number,
+        required: [true, "price is required for a meal"]
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('meal', userSchema);
