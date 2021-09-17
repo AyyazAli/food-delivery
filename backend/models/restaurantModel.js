62c9abc4e8c0cc1c89130c3984e23c4324b81168
Ayyaz Ali Furqan <ayyazalifurqan@gmail.com>
2021-09-17 18:53:21 +0500

    Create Restaurant Bug Fix

--- backend/models/restaurantModel.js
+++ backend/models/restaurantModel.js
@@ -4,11 +4,11 @@ const userSchema = mongoose.Schema({
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user',
-        require
+        required: true
     },
     name: {
         type: String,
-        require: [true, 'Restraunt Name is Required']
+        required: [true, 'Restraunt Name is Required']
     },
     description: { type: String },
     mealType: {
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
             values: ['seaFood', 'chineese', 'fastFood'],
             message: `The restaurant can only offer 'seaFood', 'chineese', 'fastFood'`
         },
-        require: [true, "Restraunt should specify the type of meal its offer"]
+        required: [true, "Restraunt should specify the type of meal its offer"]
     }
 }, { timestamps: true });
 
