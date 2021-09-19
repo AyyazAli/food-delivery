9f640bfc49309b330244083b5492e03a5ff0e329
Ayyaz Ali Furqan <ayyazalifurqan@gmail.com>
2021-09-19 22:02:16 +0500

    Added Users & Block Functionality

---
+++ backend/controllers/userController.js
@@ -0,0 +1,32 @@
+const catchAsync = require('./errorController').catchAsync;
+const AppError = require('../utils/appError');
+const UserModel = require('../models/userModel');
+const apiFeatures = require('../utils/apiFeatures.js')
+
+exports.getUsers = catchAsync(async (req, res, next) => {
+
+    const users = await new apiFeatures(UserModel.find(),
+        { role: "user" })
+        .filter()
+        .fieldsLimiting()
+        .sort()
+        .pagination()
+        .query
+
+    res.status(200).json({
+        message: 'Fetched Orders successfully',
+        data: users,
+        total: users.length
+    });
+})
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+
+    const { accountStatus } = req.body;
+    console.log(req.body)
+    const user = await UserModel.findByIdAndUpdate(req.params.id, { accountStatus }, { new: true, runValidators: true })
+    res.status(200).json({
+        message: 'User Status Updated Successfully',
+        user
+    })
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const { getUsers, updateUser } = require("../controllers/userController");
+const check_auth = require("../middlewares/check_auth");
+const { restrictTo } = require("../middlewares/restrict-to");
+const { Role } = require("../utils/constants");
 const router = express.Router();
-const {createUser, login} = require('./../controllers/authController')
+const { createUser, login } = require('./../controllers/authController')
 
 router.route('/sign-up').post(createUser)
 router.route('/login').post(login)
+
+router.route('/')
+    .get(check_auth, restrictTo(Role.owner), getUsers)
+router.route('/:id')
+    .patch(check_auth, restrictTo(Role.owner), updateUser)
 
 module.exports = router;
